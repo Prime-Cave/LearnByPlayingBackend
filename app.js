1f84b67297b4041a1bf41b7eb5e1aaa007b83ff5
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-02-28 02:17:33 +0200

    actual database setup

--- app.js
+++ app.js
@@ -3,102 +3,84 @@ const sdk = require("node-appwrite");
 const client = new sdk.Client();
 
 client
-  .setEndpoint("")
-  .setProject("") // Replace with your actual project ID
-  .setKey(
-    ""
-  ); // Replace with your actual API key
+  .setEndpoint(process.env.APIENDPOINT)
+  .setProject(process.env.PROJECT_ID) // Replace with your actual project ID
+  .setKey(process.env.APIKEY); // Replace with your actual API key
   const databases = new sdk.Databases(client);
 
-  let todoDatabase; 
-  let todoCollection;
-  
-  async function prepareDatabase() {
-      todoDatabase = await databases.create(
-          sdk.ID.unique(),
-          'TodosDB'
-      );
-  
-      todoCollection = await databases.createCollection(
-          todoDatabase.$id,
-          sdk.ID.unique(),
-          'Todos'
-      );
-  
-      await databases.createStringAttribute(
-          todoDatabase.$id,
-          todoCollection.$id,
-          'title',
-          255,
-          true
-      );
-  
-      await databases.createStringAttribute(
-          todoDatabase.$id,
-          todoCollection.$id,
-          'description',
-          255, false,
-          'This is a test description'
-      );
-      await databases.createBooleanAttribute(
-          todoDatabase.$id,
-          todoCollection.$id,
-          'isComplete',
-          true
-      );
-  }
-  
-  async function seedDatabase() {
-    var testTodo1 = {
-        title: 'Buy apples',
-        description: 'At least 2KGs',
-        isComplete: true
-    };
+let forLearners; 
+let learnersCollection;
+let gamesCollection;
 
-    var testTodo2 = {
-        title: 'Wash the apples',
-        isComplete: true
-    };
-
-    var testTodo3 = {
-        title: 'Cut the apples',
-        description: 'Don\'t forget to pack them in a box',
-        isComplete: false
-    };
-
-    await databases.createDocument(
-        todoDatabase.$id,
-        todoCollection.$id,
+async function prepareDatabase() {
+    forLearners = await databases.create(
         sdk.ID.unique(),
-        testTodo1
+        'ForLearners'
     );
-    await databases.createDocument(
-        todoDatabase.$id,
-        todoCollection.$id,
+    learnersCollection = await databases.createCollection(
+        forLearners.$id,
         sdk.ID.unique(),
-        testTodo2
+        'LearnersCollection'
+    )
+    await databases.createStringAttribute(
+        forLearners.$id,
+        learnersCollection.$id,
+        'firstName',
+        150,
+        true
+    )
+    await databases.createStringAttribute(
+        forLearners.$id,
+        learnersCollection.$id,
+        'lastName',
+        150,
+        true
+    )
+    await databases.createEmailAttribute(
+        forLearners.$id,
+        learnersCollection.$id,
+        'Email', // key
+        true, // required
     );
-    await databases.createDocument(
-        todoDatabase.$id,
-        todoCollection.$id,
+    await databases.createEnumAttribute(
+        forLearners.$id,
+        learnersCollection.$id,
+        'learnerType', // key
+        [], // elements
+        false, // required
+    );
+    await databases.createStringAttribute(
+        forLearners.$id,
+        learnersCollection.$id,
+        'password',
+        255,
+        false
+    )
+    //The Game Collection
+    gamesCollection = await databases.createCollection(
+        forLearners.$id,
         sdk.ID.unique(),
-        testTodo3
-    );
+        'GamesCollection'
+    )
+    await databases.createStringAttribute(
+        forLearners.$id,
+        gamesCollection.$id,
+        'gameId',
+        255,
+        true
+    )
+    await databases.createStringAttribute(
+        forLearners.$id,
+        gamesCollection.$id,
+        'subject',
+        255,
+        true
+    )
+    await databases.createStringAttribute(
+        forLearners.$id,
+        gamesCollection.$id,
+        'gameType',
+        10,
+        true
+    )
 }
-async function getTodos() {
-    var todos = await databases.listDocuments(
-        todoDatabase.$id,
-        todoCollection.$id
-    );
-
-    todos.documents.forEach(todo => {
-        console.log(`Title: ${todo.title}\nDescription: ${todo.description}\nIs Todo Complete: ${todo.isComplete}\n\n`);
-    });
-}
-
-async function runAllTasks() {
-    await prepareDatabase();
-    await seedDatabase();
-    await getTodos();
-}
-runAllTasks();
